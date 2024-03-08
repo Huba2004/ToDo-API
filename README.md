023ea509d9e9897b7c0ae6d1a90643c29f4be546
Your Name <huba200409@gmail.com>
2024-03-08 23:02:04 +0100

    app.php kezdeti definialasa, adatbazis es tablak letrehozasa, OSSZES es UJ route

--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 PHP: 8.3.3
 Composer: 2.7.1
 
-#Require slim/slim (4.13)
-
-#php.ini fájlban engedélyezni kell: //Ismertette: Márkus András
-extension=pdo_sqlite
-extension=sqlite3
+#Require slim/slim (4.0) //https://www.slimframework.com
+#Nyholm PSR-7 Factory + ServerRequest Creator
+#Sqlite3
--- app.php
+++ app.php
@@ -1,20 +1,44 @@
 <?php
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
+
 use Slim\Factory\AppFactory;
-
 require __DIR__ . "/vendor/autoload.php";
-
 $app = AppFactory::create();
 
-$app->get("/hello/{name}", function (
-    Request $request,
-    Response $response,
-    array $args
-) {
-    $name = $args["name"];
-    $response->getBody()->write("Hello, $name");
-    return $response;
+$adatbazisUtvonal = __DIR__ . "/data/todo.db";
+if (!file_exists($adatbazisUtvonal)) {
+    $adatbazis = new SQLite3($adatbazisUtvonal);
+    $adatbazis->exec(
+        "CREATE TABLE IF NOT EXISTS todok (id INTEGER PRIMARY KEY, teendo TEXT, kategoria TEXT)"
+    );
+} else {
+    $adatbazis = new SQLite3($adatbazisUtvonal);
+}
+
+//Route-ok
+
+//ÖSSZES
+$app->get("/todok", function ($request, $response, $args) use ($adatbazis) {
+    $todok = [];
+
+    $a = $adatbazis->query("SELECT * FROM teendo");
+    while ($sor = $a->fetchArray(SQLITE3_ASSOC)) {
+        $todok[] = $sor;
+    }
+
+    return $response->withJson($todok); //JSON-ban adatok visszad.
+});
+
+$app->post("/teendok", function ($request, $response, $args) use ($adatbazis) {
+    $adatok = $request->getParsedBody();
+
+    $lekerdezes = $adatbazis->prepare(
+        "INSERT INTO teendo (feladat, kategoria) VALUES (:feladat, :kategoria)"
+    );
+    $lekerdezes->bindValue(":feladat", $adatok["feladat"]);
+    $lekerdezes->bindValue(":kategoria", $adatok["kategoria"]);
+    $lekerdezes->execute();
+
+    return $response->withJson(["uzenet" => "Új teendő létrehozva"]);
 });
 
 $app->run();
