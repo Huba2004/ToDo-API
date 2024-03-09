d9eebf58f8a58498973db678de31c3b7b627059e
Your Name <huba200409@gmail.com>
2024-03-09 12:56:30 +0100

    Hibajavitas

--- app.php
+++ app.php
@@ -1,90 +1,87 @@
 <?php
 
 use Slim\Factory\AppFactory;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 
 require __DIR__ . "/vendor/autoload.php";
 
 $app = AppFactory::create();
 
+// Adatbázis létrehozása és inicializálása
 $adatbazisUtvonal = __DIR__ . "/todo.db";
+$adatbazisLetezik = file_exists($adatbazisUtvonal);
 
-if (!file_exists($adatbazisUtvonal)) {
-    $adatbazis = new SQLite3($adatbazisUtvonal);
-
+$adatbazis = new SQLite3($adatbazisUtvonal);
+if (!$adatbazisLetezik) {
     $adatbazis->exec(
         "CREATE TABLE IF NOT EXISTS todok (id INTEGER PRIMARY KEY, teendo TEXT, kategoria TEXT)"
     );
-
     $adatbazis->exec(
         'INSERT INTO todok (teendo, kategoria) VALUES ("Bevásárlás", "Fontos")'
     );
-
     $adatbazis->exec(
         'INSERT INTO todok (teendo, kategoria) VALUES ("Tanulás", "Sürgős")'
     );
-} else {
-    $adatbazis = new SQLite3($adatbazisUtvonal);
 }
 
-// ÖSSZES
-$app->get("/todok", function ($request, $response, $args) use ($adatbazis) {
+// Összes TODO lekérdezése
+$app->get("/todok", function (Request $request, Response $response) use (
+    $adatbazis
+) {
     $todok = [];
     $lekerdez = $adatbazis->query("SELECT * FROM todok");
-
     while ($sor = $lekerdez->fetchArray(SQLITE3_ASSOC)) {
-        array_push($todok, $sor);
+        $todok[] = $sor;
     }
-
-    $response = $response->withJson($todok);
-    return $response;
+    $payload = json_encode($todok);
+    $response->getBody()->write($payload);
+    return $response->withHeader("Content-Type", "application/json");
 });
 
-// ÚJ ELEM
-$app->post("/ujtodok", function ($request, $response, $args) use ($adatbazis) {
-    $adatok = $request->getParsedBody();
-
+// Új TODO hozzáadása
+$app->post("/ujtodok", function (Request $request, Response $response) use (
+    $adatbazis
+) {
+    $adatok = (array) $request->getParsedBody();
     $lekerdezes = $adatbazis->prepare(
         "INSERT INTO todok (teendo, kategoria) VALUES (:teendo, :kategoria)"
     );
-
     $lekerdezes->bindValue(":teendo", $adatok["teendo"]);
     $lekerdezes->bindValue(":kategoria", $adatok["kategoria"]);
     $lekerdezes->execute();
-
-    $response = $response->withJson(["uzenet" => "Új teendő létrehozva"]);
-    return $response;
+    return $response->withJson(["uzenet" => "Új teendő létrehozva"]);
 });
 
-// MÓDOSÍTÁS
-$app->put("/todok/:id", function ($request, $response, $args) use ($adatbazis) {
+// TODO módosítása
+$app->put("/todok/{id}", function (
+    Request $request,
+    Response $response,
+    array $args
+) use ($adatbazis) {
     $id = $args["id"];
-    $adatok = $request->getParsedBody();
-
+    $adatok = (array) $request->getParsedBody();
     $lekerdezes = $adatbazis->prepare(
         "UPDATE todok SET teendo = :teendo, kategoria = :kategoria WHERE id = :id"
     );
-
     $lekerdezes->bindValue(":teendo", $adatok["teendo"]);
     $lekerdezes->bindValue(":kategoria", $adatok["kategoria"]);
     $lekerdezes->bindValue(":id", $id);
     $lekerdezes->execute();
-
-    $response = $response->withJson(["uzenet" => "Teendő módosítva"]);
-    return $response;
+    return $response->withJson(["uzenet" => "Teendő módosítva"]);
 });
 
-// TÖRLÉS
-$app->delete("/todok/:id", function ($request, $response, $args) use (
-    $adatbazis
-) {
+// TODO törlése
+$app->delete("/todok/{id}", function (
+    Request $request,
+    Response $response,
+    array $args
+) use ($adatbazis) {
     $id = $args["id"];
-
     $lekerdezes = $adatbazis->prepare("DELETE FROM todok WHERE id = :id");
     $lekerdezes->bindValue(":id", $id);
     $lekerdezes->execute();
-
-    $response = $response->withJson(["uzenet" => "Teendő törölve"]);
-    return $response;
+    return $response->withJson(["uzenet" => "Teendő törölve"]);
 });
 
 $app->run();
