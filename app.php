df54a5a90a1ad8ec47baf3de46da45a283dbf043
Your Name <huba200409@gmail.com>
2024-03-09 01:02:46 +0100

    Index.html, alapveto funckiok

--- app.php
+++ app.php
@@ -6,16 +6,19 @@ require __DIR__ . "/vendor/autoload.php";
 
 $app = AppFactory::create();
 
-$adatbazisUtvonal = __DIR__ . "/data/todo.db";
+$adatbazisUtvonal = __DIR__ . "/todo.db";
 
 if (!file_exists($adatbazisUtvonal)) {
     $adatbazis = new SQLite3($adatbazisUtvonal);
+
     $adatbazis->exec(
         "CREATE TABLE IF NOT EXISTS todok (id INTEGER PRIMARY KEY, teendo TEXT, kategoria TEXT)"
     );
+
     $adatbazis->exec(
         'INSERT INTO todok (teendo, kategoria) VALUES ("Bevásárlás", "Fontos")'
     );
+
     $adatbazis->exec(
         'INSERT INTO todok (teendo, kategoria) VALUES ("Tanulás", "Sürgős")'
     );
@@ -26,13 +29,14 @@ if (!file_exists($adatbazisUtvonal)) {
 // ÖSSZES
 $app->get("/todok", function ($request, $response, $args) use ($adatbazis) {
     $todok = [];
+    $lekerdez = $adatbazis->query("SELECT * FROM todok");
 
-    $a = $adatbazis->query("SELECT * FROM todok");
-    while ($sor = $a->fetchArray(SQLITE3_ASSOC)) {
-        $todok[] = $sor;
+    while ($sor = $lekerdez->fetchArray(SQLITE3_ASSOC)) {
+        array_push($todok, $sor);
     }
 
-    return $response->withJson($todok); // Válaszként JSON-t küldünk vissza
+    $response = $response->withJson($todok);
+    return $response;
 });
 
 // ÚJ ELEM
@@ -42,32 +46,35 @@ $app->post("/ujtodok", function ($request, $response, $args) use ($adatbazis) {
     $lekerdezes = $adatbazis->prepare(
         "INSERT INTO todok (teendo, kategoria) VALUES (:teendo, :kategoria)"
     );
+
     $lekerdezes->bindValue(":teendo", $adatok["teendo"]);
     $lekerdezes->bindValue(":kategoria", $adatok["kategoria"]);
     $lekerdezes->execute();
 
-    return $response->withJson(["uzenet" => "Új teendő létrehozva"]);
+    $response = $response->withJson(["uzenet" => "Új teendő létrehozva"]);
+    return $response;
 });
-//MÓDOSÍTÁS
-$app->put("/todok/{id}", function ($request, $response, $args) use (
-    $adatbazis
-) {
+
+// MÓDOSÍTÁS
+$app->put("/todok/:id", function ($request, $response, $args) use ($adatbazis) {
     $id = $args["id"];
     $adatok = $request->getParsedBody();
 
     $lekerdezes = $adatbazis->prepare(
         "UPDATE todok SET teendo = :teendo, kategoria = :kategoria WHERE id = :id"
     );
+
     $lekerdezes->bindValue(":teendo", $adatok["teendo"]);
     $lekerdezes->bindValue(":kategoria", $adatok["kategoria"]);
     $lekerdezes->bindValue(":id", $id);
     $lekerdezes->execute();
 
-    return $response->withJson(["uzenet" => "Teendő módosítva"]);
+    $response = $response->withJson(["uzenet" => "Teendő módosítva"]);
+    return $response;
 });
 
-//TÖRLÉS
-$app->delete("/todok/{id}", function ($request, $response, $args) use (
+// TÖRLÉS
+$app->delete("/todok/:id", function ($request, $response, $args) use (
     $adatbazis
 ) {
     $id = $args["id"];
@@ -76,7 +83,8 @@ $app->delete("/todok/{id}", function ($request, $response, $args) use (
     $lekerdezes->bindValue(":id", $id);
     $lekerdezes->execute();
 
-    return $response->withJson(["uzenet" => "Teendő törölve"]);
+    $response = $response->withJson(["uzenet" => "Teendő törölve"]);
+    return $response;
 });
 
 $app->run();
